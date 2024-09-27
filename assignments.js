fe6bb6e490a94c86d7831593593d23c488ee64dd
santosh poudel <poudelsantosh002@gmail.com>
2024-09-28 05:35:52 +0545

    today 24th sept 2024

--- assignments.js
+++ assignments.js
@@ -1,25 +1,44 @@
+//##javascript program to calculat percentage of the student and division based on the same
+// -Description: crate a varibale to store a number between 0 to 500
+// -consider 500 as total value 
+// - calculate percentage and assign to a varibale
+// - get the division based on the following:
+//     -percentage >=80 ====> division is: Distinction
+//     -percentage >=60 but less than 80 ====> division is: First division
+//     -percentage >=45 but less than 60 ====> division is: Second division
+//     -percentage >=35 but less than 45 ====> division is: third division
+//     -percentage <35 =====> division is: fail
+//     -print the obtained division
 
-let obtainedMarks = 300;
-let division = (obtainedMarks / 500) * 100;
-if (obtainedMarks < 0 || obtainedMarks > 500) {
-    console.log("please enter obtained marks between 0 to 500 only");
+const marksObt = 340;
+const total = 500;
+const per = marksObt / total * 100;
+if (per >= 80) {
+    console.log("Distinction");
 }
-else if (division >= 80) {
-    console.log("first division");
+else if (per >= 60) {
+    console.log("First Division")
 }
-else if (division >= 60) {
-    console.log("second division")
+else if (per >= 45) {
+    console.log("Second division")
 }
-else if (division >= 45) {
-    console.log("third division")
+else if (per >= 35) {
+    console.log("Third Division")
 }
 else {
-    console.log("fail");
-
+    console.log("failed")
 }
+
+
 //-------------------------------------------------------------------------
 
-// calculate electricity bill
+// # calculate electricity bill 
+// - Upto 20 units lumsum amnt of npr 80
+// - for next 30 unit npr 7.5 per unit
+// - for next 50 units npr 8.6 per unit
+// - for next 100 units npr 9.5 per unit
+// - for any avobe 200 units npr 11 per unit
+
 
 // let unitConsumed = 102;
 // //let unitPrice=(20/80)*unitConsumed;
@@ -38,22 +57,50 @@ else {
 // else {
 //     console.log(`your bill is ${(unitConsumed - 20) * 11 + 80}`)
 // }
-let consumedUnit =1;
-let differenceUnit = consumedUnit- 20;
-let minPrice = 80;
-let totalBill;
-if (consumedUnit <= 20);
-totalBill = minPrice;
-if (consumedUnit >= 20 && consumedUnit <= 30)
-    totalBill = differenceUnit * 7.5 + minPrice
-if (consumedUnit >= 30 && consumedUnit <= 50);
-totalBill = differenceUnit* 8.6 + minPrice;
-if (consumedUnit>=50 && consumedUnit<=100)
-    totalBill=differenceUnit*9.5+minPrice;
-else
-totalBill=differenceUnit*11+minPrice;
-console.log(`Your total Bill is:${totalBill}`);
+let consumedUnit = 255;
+let totalBill = 0;
+if (consumedUnit <= 20) {
+    totalBill = 80;
+}
+else if (consumedUnit <= 50) {
+    totalBill = 80 + (consumedUnit - 20) * 7.5;
+}
+else if (consumedUnit <= 100) {
+    totalBill = 80 + 30 * 7.5 + (consumedUnit - 50) * 8.6
+}
+else if (consumedUnit <= 200) {
+    totalBill = 80 + 30 * 7.5 + 50 * 8.6 + (consumedUnit - 100) * 9.5
+}
+else {
+    totalBill = 80 + 30 * 7.5 + 50 * 8.6 + 100 * 9.5 + (consumedUnit - 200) * 11;
+}
+console.log("total bill is:", totalBill);
 
+/**
+ * convert celcius to farhenheit and vice versa
+ * c=>f
+ * f=(celcius *9/5)+32
+ * c=(farheinheit -32) *5/9
+ */
+
+const temp = 100 //F
+let scale = "F" // F
+
+
+// scale C ==> convert to Fahreinheit
+// scale F ==> convert to Celcius
+let convertedTemp = 0;
+if (scale.toLowerCase() === 'f') {
+    convertedTemp = (temp - 32) * 5 / 9;
+    console.log("celcius:", convertedTemp)
+}
+else if (scale.toLowerCase() === "c") {
+    convertedTemp = (temp * 9 / 5) + 32;
+    console.log("Farheinheit:", convertedTemp);
+}
+else {
+    console.log("Invalid Scale")
+}
 
 
 
--- index.js
+++ index.js
@@ -9,9 +9,11 @@ let b = 10;
 console.log(b);
 {
     let b = 20;
-   // b=20;
+    // b=20;
     console.log(b);
 
 }
 console.log(b);
+document.write("hi");
+
 
